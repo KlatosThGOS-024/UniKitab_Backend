d49e2a2e31f7568b40f660d516496d55c2b67d32
KlatosThGOS-024 <priyanshutiwari878@yahoo.com>
2025-03-05 18:36:00 +0530

    commit idk

---
+++ apps/backend/src/controllers/pdf.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler";
+
+
+const createPdf = asyncHandler(async (req:Request,res:Response)=>{
+    try {
+        const getPdf = 
+    } catch (error) {
+        
+    }
+})
---
+++ apps/backend/src/models/pdfBook.ts
@@ -0,0 +1,16 @@
+import mongoose, { model } from "mongoose";
+
+const pdfSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  pdfName: {
+    type: String,
+    required: true,
+    syncIndexes: true,
+    index: true,
+  },
+});
+const PDF = model("PDF", pdfSchema);
+export default PDF;
