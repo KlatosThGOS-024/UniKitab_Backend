18508b07582f68c0e059faf5c50aa2ba9ca71514
KlatosThGOS-024 <priyanshutiwari878@yahoo.com>
2025-03-20 14:13:55 +0530

    commit final

--- apps/backend/dist/controllers/pdf.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { Request, Response } from "express";
-// import asyncHandler from "../utils/asyncHandler";
-// const createPdf = asyncHandler(async (req:Request,res:Response)=>{
-//     try {
-//         const getPdf =
-//     } catch (error) {
-//     }
-// })
--- apps/backend/dist/routes/problem.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.problemRouter = void 0;
-const express_1 = require("express");
-const Problem_Controller_1 = require("../controllers/Problem.Controller");
-const problemRouter = (0, express_1.Router)();
-exports.problemRouter = problemRouter;
-problemRouter.route("/createQ").post(Problem_Controller_1.addProblemToDb);
-problemRouter.route("/getQ").get(Problem_Controller_1.getProblemFromDb);
