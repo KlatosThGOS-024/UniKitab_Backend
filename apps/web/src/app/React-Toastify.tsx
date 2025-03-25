c75ef4b84f1770dd25382792bdd162c6fc57ccc2
KlatosThGOS-024 <priyanshutiwari878@yahoo.com>
2025-03-25 23:19:32 +0530

    commit idk

--- apps/web/src/app/React-Toastify.tsx
+++
@@ -1,8 +0,0 @@
-// React-Toastify.tsx
-"use client";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-export const ReactToastify = () => {
-  return <ToastContainer />;
-};
